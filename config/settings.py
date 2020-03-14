d40e7a64b899f3f8f898740cba173e81c890bd28
Kouki <lle0nonnndebo@gmail.com>
2020-03-15 00:11:04 +0900

    DBをSQLiteに変更

--- config/settings.py
+++ config/settings.py
@@ -72,18 +72,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'DiaryApp',
+#         'USER': 'root',
+#         'PASSWORD': 'P@ssw0rd!',
+#         'HOST': '',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             # 制約を厳しくチェック
+#             'sql_mode': 'traditional',
+#         },
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DiaryApp',
-        'USER': 'root',
-        'PASSWORD': 'P@ssw0rd!',
-        'HOST': '',
-        'PORT': '3306',
-        'OPTIONS': {
-            # 制約を厳しくチェック
-            'sql_mode': 'traditional',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
